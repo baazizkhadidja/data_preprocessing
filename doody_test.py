29354fb304d5f7866c82bff165542698b839ce91
Baaziz <baazize.khadidja@gmail.com>
2025-08-24 16:43:07 +0200

    missing and categorical data

---
+++ doody_test.py
@@ -0,0 +1,46 @@
+
+#Import libraries
+import pandas as pd
+import numpy as np
+import matplotlib as mtp
+
+
+
+
+#Import dataset
+dataset = pd.read_csv('Data.csv')
+
+dataset
+
+X = dataset.iloc[: , :-1].values
+X
+y = dataset.iloc[: , 3]
+y
+
+#Taking care of missing data
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+
+imputer = imputer.fit(X[ : ,1:3])
+
+X[ : , 1:3] = imputer.transform((X[ : , 1:3]))
+X[ : , 1:3]
+
+#Encoding categorical data: pour transformer les pays en chiffre
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.compose import ColumnTransformer
+
+
+ct = ColumnTransformer(transformers=[("encoder",OneHotEncoder(),[0])],remainder = "passthrough")
+
+
+X = ct.fit_transform(X)
+
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
+
+y
+
+
+
