d225253a8e1b2528158adfcd1429562912c91137
Alice <gmail.com>
2025-08-25 17:39:07 +0200

    pretraitement des données avec sklearn

--- doody_test.py
+++ doody_test.py
@@ -39,8 +39,12 @@ X = ct.fit_transform(X)
 
 labelEncoder_y = LabelEncoder()
 y = labelEncoder_y.fit_transform(y)
-
+ 
 y
+#Splitting the dataset into the training set and test dataset
+
+from sklearn import cross
+
 
 
 
